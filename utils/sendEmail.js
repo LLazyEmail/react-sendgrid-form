2cb9123b3a71e25c870bd87de9d8e55cbb283d06
masterbogdan <maiskiychavk@gmail.com>
2020-12-12 14:54:24 +0200

    change fetch sending to twilo sgMail

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,39 +1,58 @@
-import fetch from 'node-fetch';
+const sgMail = require('@sendgrid/mail');
 
-const SENDGRID_API = 'https://api.sendgrid.com/v3/mail/send';
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const sendEmail = async ({ email }) => {
-    try {
-        const res = await fetch(SENDGRID_API, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              Authorization: `Bearer ${process.env.SENDGRID_API_KEY}`,
-            },
-            body: JSON.stringify({
-                from: {
-                    email: 'support@hackernoon.com',
-                    name: 'Hackernoon',
-                  },
-                personalizations: [
-                    {
-                    to: [
-                        {
-                        email,
-                        },
-                    ],
-                    subject: 'Demo success :)',
-                    },
-                ],
-                template_id: 'd-dc18336cd62d40fba8820bccb351aa8f',
-            })
-          })
-          const status = await res.text();
-          console.log(status);
-    } catch (error) {
-        console.log(error);
-    }
+const sendEmail = ({ email }) => { 
+    const msg = {
+        to: email,
+        from: 'Hackernoon <support@hackernoon.com>', // Use the email address or domain you verified above
+        subject: 'Sending with Twilio SendGrid is Fun',
+        templateId: 'd-dc18336cd62d40fba8820bccb351aa8f',
+    };
+  //ES6
   
-};
+  sgMail
+    .send(msg)
+    .then(() => {}, error => {
+      console.error(error);
+  
+      if (error.response) {
+        console.error(error.response.body)
+      }
+    });
+}
+// const sendEmail = async ({ email }) => {
+//     try {
+//         const res = await fetch(SENDGRID_API, {
+//             method: 'POST',
+//             headers: {
+//               'Content-Type': 'application/json',
+//               Authorization: `Bearer ${process.env.SENDGRID_API_KEY}`,
+//             },
+//             body: JSON.stringify({
+//                 from: {
+//                     email: 'support@hackernoon.com',
+//                     name: 'Hackernoon',
+//                   },
+//                 personalizations: [
+//                     {
+//                     to: [
+//                         {
+//                         email,
+//                         },
+//                     ],
+//                     subject: 'Demo success :)',
+//                     },
+//                 ],
+//                 template_id: 'd-dc18336cd62d40fba8820bccb351aa8f',
+//             })
+//           })
+//           const status = await res.text();
+//           console.log(status);
+//     } catch (error) {
+//         console.log(error);
+//     }
+  
+// };
 
 export { sendEmail };
