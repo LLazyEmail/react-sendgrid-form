0bdef41177d7c76227d645c5d90b795bf2560bed
masterbogdan <maiskiychavk@gmail.com>
2020-12-27 20:51:43 +0200

    [add small improvements]

--- components/Forms/BrandsAsAuthorRejected/BrandsAsAuthorRejected.js
+++ components/Forms/BrandsAsAuthorRejected/BrandsAsAuthorRejected.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Input, Button, Checkbox, Typography } from 'antd';
+import { Form, Input, Button, Typography, Image, Row, Col } from 'antd';
 
 const { Title } = Typography;
 
@@ -21,56 +21,68 @@ const BrandsAsAuthorRejected = () => {
 
   return (
     <>
-      <Title level={2}>Brand as Author Story Rejected</Title>
+      <Row justify="center" align="middle">
+        <Title level={2}>Brand as Author Story Rejected</Title>
+      </Row>
 
-      <Form name="basic" layout="vertical" onFinish={onFinish}>
-        <Form.Item label="type" hidden name="type" initialValue={7}>
-          <Input />
-        </Form.Item>
+      <Row>
+        <Col xs={{ span: 20, offset: 2 }} lg={{ span: 10, offset: 1 }}>
+          <Form name="basic" layout="vertical" onFinish={onFinish}>
+            <Form.Item label="type" hidden name="type" initialValue={7}>
+              <Input />
+            </Form.Item>
 
-        <Form.Item
-          label="Email"
-          name="email"
-          rules={[
-            {
-              required: true,
-              message: 'Email is required!'
-            }
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Company Name"
-          name="name"
-          rules={[
-            {
-              required: true,
-              message: 'Company Name is required!'
-            }
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Story Title"
-          name="storyTitle"
-          rules={[
-            {
-              required: true,
-              message: 'Story Title is required!'
-            }
-          ]}
-        >
-          <Input />
-        </Form.Item>
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: 'Email is required!'
+                }
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Company Name"
+              name="name"
+              rules={[
+                {
+                  required: true,
+                  message: 'Company Name is required!'
+                }
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Story Title"
+              name="storyTitle"
+              rules={[
+                {
+                  required: true,
+                  message: 'Story Title is required!'
+                }
+              ]}
+            >
+              <Input />
+            </Form.Item>
 
-        <Form.Item>
-          <Button type="primary" htmlType="submit" block>
-            Submit form
-          </Button>
-        </Form.Item>
-      </Form>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" block>
+                Submit form
+              </Button>
+            </Form.Item>
+          </Form>
+        </Col>
+        <Col xs={{ span: 20, offset: 2 }} lg={{ span: 10 }}>
+          <Title level={4}>Example</Title>
+          <Image
+            src="/form-images/baa-story-rejected.png"
+          />
+        </Col>
+      </Row>
     </>
   );
 };
